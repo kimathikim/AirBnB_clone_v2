a71fda316317c277b27a9195c77afc331115ba32
Brian Kimathi M <briankimathi94@gmail.com>
2024-01-01 18:04:56 +0300

    variables in url

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that runs flask and assigns variables for url creation"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """function that returns hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """function that returns hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """function that returns c and text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
