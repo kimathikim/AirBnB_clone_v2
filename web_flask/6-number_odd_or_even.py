89cb637f0363d6dce7210156dd41a60c5774f830
Brian Kimathi M <briankimathi94@gmail.com>
2024-01-01 20:54:47 +0300

    check if even or odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -36,8 +36,9 @@ def number(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
+    """function that returns number and n"""
     return render_template("5-number.html", n=n)
 
 
