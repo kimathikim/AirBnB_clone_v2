f66b607b0d793ef14bc740809a7b78743bf2991d
Brian Kimathi M <briankimathi94@gmail.com>
2024-01-01 19:33:13 +0300

    check if variable is a number

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,63 @@
 #!/usr/bin/python3
-"""script that runs flask and assigns variables for url creation"""
+"""
+Script that starts a Flask web application
+"""
+
+
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """function that returns hello"""
+    """
+    Class hello, first test class
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """function that returns hbnb"""
+    """
+    Class hbnb, second test class
+    """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """function that returns c and text"""
-    return "C {}".format(text.replace("_", " "))
+def c_route(text):
+    """
+    Class c_route, thrird test class
+    """
+    return "C " + str(text.replace("_", " "))
 
 
 @app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python(text):
-    """function that returns python and text"""
-    return "Python {}".format(text.replace("_", " "))
+def python_route(text):
+    """
+    Class python_route, fourth test class
+    """
+    return "Python " + str(text.replace("_", " "))
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    """function that returns number and n"""
-    return "{} is a number".format(n)
+def number_route(n):
+    """
+    Class number_route, fifth test class
+    """
+    return str(n) + " is a number"
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    """function that returns number and n"""
-    return render_template("5-number.html", n=n)
+def n_template(n):
+    """
+    Class n_template, sixth test class
+    """
+    return render_template("5-number.html", num=n)
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
