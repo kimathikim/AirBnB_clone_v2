4e80ffad04cb7a62dc4669fd449c5e0baab0604d
Brian Kimathi M <briankimathi94@gmail.com>
2024-01-01 16:49:51 +0300

    running flask for the first time

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# Write a script that starts a Flask web application on port 5000 and
-# displays Hello HBNB!
+"""Write a script that starts a Flask web application on port 5000 and
+displays Hello HBNB!
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +9,9 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """
+    return hello ...
+    """
     return "Hello HBNB!"
 
 
